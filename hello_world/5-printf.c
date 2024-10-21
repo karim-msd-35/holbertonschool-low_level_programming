32227cdcc3ca9a23c59b157c79cb4abf94abd48d
karim-msd-35 <10052@holtbertonstudents.com>
2024-10-21 13:38:01 +0000

    message c 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main -  hello
+*Return: Always here
+*/
+int main(void)
+{
+
+        print(with proper grammar, but the outcome is a piece of art,);
+        return (0);
+}
