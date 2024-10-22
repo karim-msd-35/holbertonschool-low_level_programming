c2d78204c95c9ac729a322c602a413c763231139
karim-msd-35 <10052@hotlbertonstudents.com>
2024-10-22 08:07:25 +0000

    oui

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,10 +9,10 @@
 
 int main()
 {
-    printf("Taille d'un char : %lu octets\n", sizeof(char));
-    printf("Taille d'un int : %lu octets\n", sizeof(int));
-    printf("Taille d'un long : %lu octets\n", sizeof(long));
-    printf("Taille d'un long long : %lu octets\n", sizeof(long long));
-    printf("Taille d'un float : %lu octets\n", sizeof(float));
+    printf("Taille d'un char : %lu byte(s)\n", sizeof(char));
+    printf("Taille d'un int : %lu byte(s)\n", sizeof(int));
+    printf("Taille d'un long : %lu byte(s)\n", sizeof(long));
+    printf("Taille d'un long long : %lu byte(s)\n", sizeof(long long));
+    printf("Taille d'un float : %lu byte(s)\n", sizeof(float));
     return (0);
 }
