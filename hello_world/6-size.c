9d7340d07aff4764d479fd606168cbefd3197c83
karim-msd-35 <10052@hotlbertonstudents.com>
2024-10-22 07:43:43 +0000

    5

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+* main -entry
+*
+*
+* return operation was good
+/*
+
+int main() {
+    printf("Taille d'un char : %zu octets\n", sizeof(char));
+    printf("Taille d'un int : %zu octets\n", sizeof(int));
+    printf("Taille d'un long : %zu octets\n", sizeof(long));
+    printf("Taille d'un long long : %zu octets\n", sizeof(long long));
+    printf("Taille d'un float : %zu octets\n", sizeof(float));
+    printf("Taille d'un double : %zu octets\n", sizeof(double));
+    return 0;
