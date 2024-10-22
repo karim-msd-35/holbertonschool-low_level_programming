6b9a0ad7c5ea6d09e75de08de42e4808881c602b
karim-msd-35 <10052@hotlbertonstudents.com>
2024-10-22 08:42:59 +0000

    message

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
 * Return -T
 */ 
 int main() {
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a long: %zu byte(s)\n", sizeof(long));
-    printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
-    printf("Size of a double: %zu byte(s)\n", sizeof(double));
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+    printf("Size of a double: %lu byte(s)\n", sizeof(double));
     return 0;
 }
