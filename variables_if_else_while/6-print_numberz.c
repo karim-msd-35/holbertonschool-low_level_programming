6ae2833432f35b24c3e9fda85ec78fc1ab21cb1e
karim-msd-35 <10052@holbertonstudents.com>
2024-10-23 12:04:49 +0000

    msg

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - hello
+*Return: '0' success
+*/
+int main(void)
+{
+	int number
+
+	for (number = 0; number <= 9; number++)
+{
+	putchar("%d", number);
+}
+	putchar('\n');
+	return (0);
+}
