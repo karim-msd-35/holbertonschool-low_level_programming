f363023abc9e44a956d07ba14c1237d95b30ce4c
karim-msd-35 <10052@holbertonstudents.com>
2024-10-23 07:59:02 +0000

    message

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@ int main(void)
 {
 char letter;
 
-for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 {
-putchar(letter);
+	putchar(letter);
 }
-for (letter = 'A'; letter <= 'Z'; letter++)}
+	for (letter = 'A'; letter <= 'Z'; letter++)}
 {
-putchar(letter);
+	putchar(letter);
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
