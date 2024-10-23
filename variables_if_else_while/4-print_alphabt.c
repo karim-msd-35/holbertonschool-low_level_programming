cf4f9047dfba3eceba6b174f00c689faa502b9bc
karim-msd-35 <10052@holbertonstudents.com>
2024-10-23 08:36:00 +0000

    message

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,11 @@ int main(void)
 {
         char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
-        if (letter != 'e' ; letter != 'q'; letter++)
+	for (letter = 'a';letter <= 'z'; letter++)
+{
+	putchar(letter);
+}
+	if (letter != 'e';letter != 'q'; letter++)
 
         putchar('\n');
         return (0);
