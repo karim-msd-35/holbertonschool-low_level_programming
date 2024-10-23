a71edfab51b6d42b3493b6f34bd940db1d9065b0
karim-msd-35 <10052@holbertonstudents.com>
2024-10-23 09:44:26 +0000

    busy

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
 * main = 0
+* hell yeah
 *Return: yes
 */
 
 int main(void)
 {
 	char number;
+
 	for (number '0'; number <= '9' number++)
+
 {	puchar ('number');
 }
 	puchar ('\n')
