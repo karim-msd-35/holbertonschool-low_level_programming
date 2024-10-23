b3e70e3765e54792f2bd91b1932f69f0a587d036
karim-msd-35 <10052@holbertonstudents.com>
2024-10-23 13:54:36 +0000

    msg

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	for (number = 0; number < 10; number++)
 {
 	putchar (number + '0');
-}
 
 	if (number < 9)
 {
 	putchar (',');
 	putchar (' ');
+}
 }
 	putchar ('\n');
 	return (0);
